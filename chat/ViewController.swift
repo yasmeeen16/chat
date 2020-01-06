9f55a2db9ef90e866863ca264ae208b3fcc332f6
amr <amrKhalefa@bitbucket.org>
2020-01-06 14:28:55 +0200

    Initial Commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by Atiaf on 1/6/20.
+//  Copyright © 2020 Atiaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
